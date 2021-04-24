2073c4b077ae1145c21a41c8bcf7f0e564fc76e6
Martin Laidma <martinlaidma@gmail.com>
2021-04-24 23:31:26 +0200

    Partial attempt for both-side cave generation.
    Will come back to this if I have time. This attempts to clear the tiles
    behind it to be able to generate a deeper going cave also in the other
    direction

--- Ludum Dare 48/Assets/Scripts/Level.cs
+++ Ludum Dare 48/Assets/Scripts/Level.cs
@@ -3,66 +3,178 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Tilemaps;
 
+struct LastColumn
+{
+    public Vector2Int ahead;
+    public Vector2Int behind;
+}
+
 public class Level : MonoBehaviour
 {
-    [SerializeField] private string seed = "evergreen";
-    [SerializeField] int worldLenght = 25;
-
-    [SerializeField] private int floorDropChance = 25;
-
+    [SerializeField] int floorDropChance = 15;
+    [SerializeField] int lookAhead = 3;
+    [SerializeField] int lookBack = 5;
     [SerializeField] TileBase tile;
 
+    [SerializeField] Camera cam;
+
     private int worldHeight = 4;
-    
-    private int floorX = 0;
-    private int floorY = 0;
+    private int spawnWidth = 6;
+
+    LastColumn lastColumn;
+    private bool spawnPassed = false;
+
+    private Dictionary<int, List<Vector2Int>> tileDict; 
 
     private System.Random rng;
-
-    private Tilemap tilemap;
     
+    private Tilemap tilemap;
+
 
     // Start is called before the first frame update
     void Start()
     {
         Init();
-        GenerateLevel();
+        GenerateSpawn();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        MoveAhead();
+        //MoveBack();
+
+        if(!spawnPassed && GetViewportBehind() - lookBack >= 0)
+        {
+            spawnPassed = true;
+            Debug.Log("Spawn passed");
+        }
+
+        Debug.Log("lastColumn.ahead: " + (lastColumn.ahead.x, lastColumn.ahead.y).ToString());
+        Debug.Log("lastColumn.behind: " + (lastColumn.behind.x, lastColumn.behind.y).ToString());
     }
 
     private void Init()
     {
         rng = new System.Random();
         tilemap = GetComponent<Tilemap>();
+        tilemap.ClearAllTiles();
+
+        tileDict = new Dictionary<int, List<Vector2Int>>();
+
+        lastColumn.ahead = new Vector2Int(0, 0);
+        lastColumn.behind = new Vector2Int(0, 0);
     }
 
-    public void GenerateLevel()
+    private void GenerateSpawn()
     {
-        for(int x = floorX; x < worldLenght; x++)
+        for(int x = 0; x < spawnWidth; x++)
         {
-            GetNextColumn();
+            Vector2Int ahead = new Vector2Int(x, 0);
+            Vector2Int behind = new Vector2Int(-x - 1, 0);
+
+            DrawColumn(ahead);
+            DrawColumn(behind);
+
+            lastColumn.ahead = ahead;
+            lastColumn.behind = behind;
+        }
+
+    }
+
+    private void MoveAhead()
+    {
+        int viewportAhead = GetViewportAhead();
+
+        // Generate Ahead
+        if (lastColumn.ahead.x - viewportAhead < lookAhead)
+        {
+            int x = lastColumn.ahead.x;
+            int y = lastColumn.ahead.y;
+
+            if(DropFloor())
+            {
+                y -= 1;   
+            }
+
+            x += 1;
+
+            Vector2Int newColumn = new Vector2Int(x, y);
+
+            DrawColumn(newColumn);
+            lastColumn.ahead = newColumn;
+        }
+
+        // Delete behind
+        if(spawnPassed)
+        {
+            int viewportBehind = GetViewportBehind();
+            int toClearFrom = viewportBehind - lookBack;
+
+            List<int> columns = new List<int>(tileDict.Keys);
+
+            foreach (int column in columns)
+            {
+                if(column < toClearFrom)
+                {
+                    //Grab the bottom-most tile of next column and set it as lastColumn.behind
+                    //that is where backward generation will start from
+                    lastColumn.behind = tileDict[column + 1][0];
+                    
+                    ClearColumn(column);
+                }
+            }
         }
     }
 
-    private void GetNextColumn()
+    private void MoveBack()
     {
-        int random = rng.Next(0, 100);
+        int viewportBehind = GetViewportBehind();
+    }
 
-        if(random < floorDropChance)
-        {
-            floorY -= 1;
-        }
+    private void DrawColumn(Vector2Int coord)
+    {
+        Debug.Log("DrawColumn");
+        List<Vector2Int> coords = new List<Vector2Int>();
 
         for(int i = 0; i < worldHeight; i++)
         {
-            tilemap.SetTile(new Vector3Int(floorX, floorY + i, 0), tile);
+            Vector2Int newTile = new Vector2Int(coord.x, coord.y + i);
+            tilemap.SetTile(new Vector3Int(newTile.x, newTile.y, 0), tile);
+            coords.Add(newTile);
         }
+        
+        tileDict.Add(coord.x, coords);
 
-        floorX += 1;
+    }
+
+    private void ClearColumn(int x)
+    {
+        List<Vector2Int> tiles = tileDict[x];
+
+        foreach (Vector2Int tile in tiles)
+        {
+            tilemap.SetTile(new Vector3Int(tile.x, tile.y, 0), null);
+        }
+        
+        tileDict.Remove(x);
+    }
+
+    private bool DropFloor()
+    {
+        int random = rng.Next(0, 100);
+
+        Debug.Log("Floordrop:" + floorDropChance);  
+        return random < floorDropChance;
+    }
+
+    private int GetViewportAhead()
+    {
+        return Mathf.FloorToInt(cam.ViewportToWorldPoint(new Vector3(1, 0, cam.nearClipPlane)).x);
+    }
+
+    private int GetViewportBehind()
+    {
+        return Mathf.FloorToInt(cam.ViewportToWorldPoint(new Vector3(0, 0, cam.nearClipPlane)).x);
     }
 }
