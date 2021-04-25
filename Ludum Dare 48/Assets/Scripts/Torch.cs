ea44eafc1dc4eedadc97b31761ac45fffc118f3b
Martin Laidma <martinlaidma@gmail.com>
2021-04-25 16:20:28 +0200

    Add flicker to torches

--- Ludum Dare 48/Assets/Scripts/Torch.cs
+++ Ludum Dare 48/Assets/Scripts/Torch.cs
@@ -2,17 +2,40 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+using UnityEngine.Experimental.Rendering.Universal;
+
 public class Torch : MonoBehaviour
 {
+
+    private const float minIntensity = 0.9f;
+    private const float maxIntensity = 1f;
+
+    [SerializeField] float minFlickerWait = 0.2f;
+    [SerializeField] float maxFlickerWait = 0.4f;
+
+    Light2D light2D;
+    System.Random rng;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        light2D = GetComponentInChildren<Light2D>();
+        rng = new System.Random();
+
+        StartCoroutine(Flicker());
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+
+    IEnumerator Flicker()
+    {   
+        while(true)
+        {
+            float flicker = Random.Range(minIntensity, maxIntensity);
+            float wait = Random.Range(minFlickerWait, maxFlickerWait);
+
+            light2D.intensity = flicker;
+
+            yield return new WaitForSeconds(wait);
+        }
     }
 }
